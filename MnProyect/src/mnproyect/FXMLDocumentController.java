20fcd5cc91695479d869814913ec4581b1fa2123
andres_002 <andres_002@192.168.1.66>
2015-12-03 03:17:45 -0600

    Agregar diferentes paneles a la interfaz

--- MnProyect/src/mnproyect/FXMLDocumentController.java
+++ MnProyect/src/mnproyect/FXMLDocumentController.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 /**
@@ -19,44 +20,60 @@ import javafx.scene.layout.Pane;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML private ComboBox cb_opcion;
-    @FXML private Label lbl_metodos;
-    @FXML private Pane pane_Bi;
+    @FXML
+    private ComboBox cb_opcion, cb_trunc;
+    @FXML
+    private Label lbl_metodos, lbl_first, lbl_b, lbl_f, lbl_n, lbl_tol, lbl_fprima;
+    @FXML
+    private Pane pane_Bi, pane_punto, pane_Newton;
+    @FXML
+    private TextField txt_a, txt_b, txt_f, txt_n, txt_tol, txt_fprima;
     
-    private void combo(){
+    private void combo() {
         pane_Bi.setVisible(false);
+        pane_Newton.setVisible(false);
+        pane_punto.setVisible(false);
         lbl_metodos.setVisible(true);
+        cb_trunc.getItems().add("Truncamiento");
+        cb_trunc.getItems().add("Redondeo");
+        cb_trunc.setValue("Redondeo");
         cb_opcion.getItems().add("Bisección");
         cb_opcion.getItems().add("Punto fijo");
         cb_opcion.getItems().add("Newton-Raphson");
         cb_opcion.getItems().add("Lagrange");
-        cb_opcion.setOnAction(a ->{
-            lbl_metodos.setVisible(false);
-            pane_Bi.setVisible(false);
+        cb_opcion.setOnAction(a -> {
+            lbl_metodos.setVisible(false);            
             String opc = cb_opcion.getValue().toString();
-            switch(opc){
+            pane_Bi.setVisible(false);
+            pane_Newton.setVisible(false);
+            pane_punto.setVisible(false);
+            switch (opc) {
                 case "Bisección":
                     pane_Bi.setVisible(true);
                     System.out.println("Biseccion");
                     break;
                 case "Punto fijo":
+                    pane_punto.setVisible(true);
+                    txt_a.setPromptText("Introduzca el valor inicial de X");
+                    
                     System.out.println("Punto Fijo");
                     break;
                 case "Newton-Raphson":
+                    pane_Newton.setVisible(true);
                     System.out.println("Newton");
                     break;
                 case "Lagrange":
+                    pane_Bi.setVisible(true);
                     System.out.println("Lagrange");
                     break;
             }
             
-                });
-        
+        });
     }
-
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         combo();
-    }    
+    }
     
 }
