23c1d914cf3034102d417f568edf4c627f44823e
andres_002 <andres_002@192.168.1.66>
2015-12-04 12:01:57 -0600

    cambios en el proyecto

--- MnProyect/src/mnproyect/FXMLDocumentController.java
+++ MnProyect/src/mnproyect/FXMLDocumentController.java
@@ -6,9 +6,12 @@
 package mnproyect;
 
 import java.net.URL;
+import java.util.Iterator;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -19,17 +22,19 @@ import javafx.scene.layout.Pane;
  * @author andres_002
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private ComboBox cb_opcion, cb_trunc;
+    private ComboBox cb_opcion, cb_trunc, cb_paro;
     @FXML
     private Label lbl_metodos, lbl_first, lbl_b, lbl_f, lbl_n, lbl_tol, lbl_fprima;
     @FXML
     private Pane pane_Bi, pane_punto, pane_Newton;
     @FXML
-    private TextField txt_a, txt_b, txt_f, txt_n, txt_tol, txt_fprima;
-    
+    private TextField txt_aBi, txt_bBi, txt_fBi, txt_nBi,txt_nNewton,txt_tolBi, txt_tolNewton, txt_fprima;
+
     private void combo() {
+        txt_tolBi.setDisable(true);
+        txt_nBi.setDisable(true);
         pane_Bi.setVisible(false);
         pane_Newton.setVisible(false);
         pane_punto.setVisible(false);
@@ -37,12 +42,24 @@ public class FXMLDocumentController implements Initializable {
         cb_trunc.getItems().add("Truncamiento");
         cb_trunc.getItems().add("Redondeo");
         cb_trunc.setValue("Redondeo");
+        cb_paro.getItems().add("Iteraciones");
+        cb_paro.getItems().add("Tolerancia");
+        cb_paro.setValue("Tolerancia");
         cb_opcion.getItems().add("Bisección");
         cb_opcion.getItems().add("Punto fijo");
         cb_opcion.getItems().add("Newton-Raphson");
         cb_opcion.getItems().add("Lagrange");
+        cb_paro.setOnAction(a -> {
+            if (cb_paro.getValue().equals("Tolerancia")) {
+                txt_nBi.setDisable(true);
+                txt_tolBi.setDisable(false);
+            } else {
+                txt_tolBi.setDisable(true);
+                txt_nBi.setDisable(false);
+            }
+        });
         cb_opcion.setOnAction(a -> {
-            lbl_metodos.setVisible(false);            
+            lbl_metodos.setVisible(false);
             String opc = cb_opcion.getValue().toString();
             pane_Bi.setVisible(false);
             pane_Newton.setVisible(false);
@@ -54,8 +71,8 @@ public class FXMLDocumentController implements Initializable {
                     break;
                 case "Punto fijo":
                     pane_punto.setVisible(true);
-                    txt_a.setPromptText("Introduzca el valor inicial de X");
-                    
+                    //txt_a.setPromptText("Introduzca el valor inicial de X");
+
                     System.out.println("Punto Fijo");
                     break;
                 case "Newton-Raphson":
@@ -67,13 +84,35 @@ public class FXMLDocumentController implements Initializable {
                     System.out.println("Lagrange");
                     break;
             }
-            
+
         });
     }
     
+    @FXML
+    private void bisecc() {
+        String a = txt_aBi.getText().replace(" ", "");
+        String b = txt_bBi.getText().replace(" ", "");
+        String function = txt_fBi.getText().replace(" ", "");
+        function = function.toUpperCase();
+        String functiona = function.replace("X", a+"");
+        if(!(a.matches("[0-9]*") && b.matches("[0-9]*")) || (a.isEmpty()||b.isEmpty() || function.isEmpty())){
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setHeaderText("ENTRADA INVALIDA");
+            alert.setTitle("ERROR");
+            alert.setContentText("Los campos A y B deben ser solo numeros y f(x) debe ser una entrada válida");
+            alert.showAndWait();
+        }
+        Biseccion bi = new Biseccion();
+        bi.funcion(functiona);
+    }
+    
+    
+    private void parser(String function, double a){
+        
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         combo();
     }
-    
+
 }
