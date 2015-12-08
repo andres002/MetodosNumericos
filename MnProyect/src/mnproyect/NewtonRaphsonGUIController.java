8b3b9070ecc6762d3e08a0a50cf3d4b11bd39c82
andres_002 <andres_002@192.168.1.66>
2015-12-08 12:05:28 -0600

    Abrir Newton Raphson

--- MnProyect/src/mnproyect/NewtonRaphsonGUIController.java
+++ MnProyect/src/mnproyect/NewtonRaphsonGUIController.java
@@ -5,13 +5,21 @@
  */
 package mnproyect;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -23,6 +31,30 @@ public class NewtonRaphsonGUIController implements Initializable {
     @FXML private ComboBox IteraTol,serultOp;
     @FXML private TextField XEntry,funcionEntry,derivadaEntry,nEntry,tolEntry;
     @FXML private TextArea textArea;
+    
+    
+        @FXML
+    public void openWindowBack(ActionEvent e) {
+        openWindowWithOption("Principal.fxml");
+    }
+    
+    private void openWindowWithOption(String file) {
+         
+        Stage stage2 = (Stage) XEntry.getScene().getWindow();
+        stage2.close();
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource(file));
+        } catch (IOException ex) {
+            Logger.getLogger(BiseccionGUIController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
+    }
     
     /**
      * Initializes the controller class.
