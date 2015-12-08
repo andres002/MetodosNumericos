5209610b0bacb09b80a4c5cdd294223e6c6c4cf4
Carlos ZRobles <carlitos_zenteno@hotmail.com>
2015-12-08 12:31:08 -0600

    Secante GUI Added

---
+++ MnProyect/src/mnproyect/SecanteGUIController.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mnproyect;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Karlos
+ */
+public class SecanteGUIController implements Initializable {
+    
+    @FXML private ComboBox IteraTol,serultOp;
+    @FXML private TextField x0Entry,x1Entry,funcionEntry,nEntry,tolEntry;
+    @FXML private TextArea textArea;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        IteraTol.getItems().addAll(
+        "Tolerancia","Iteraciones");
+        IteraTol.setValue("Tolerancia");
+        serultOp.getItems().addAll(
+        "Truncamiento","Redondeo");
+        serultOp.setValue("Redondeo");
+        IteraTol.setOnAction(a -> {
+            if (IteraTol.getValue().equals("Tolerancia")) {
+                nEntry.setDisable(true);
+                tolEntry.setDisable(false);
+            } else {
+                tolEntry.setDisable(true);
+                nEntry.setDisable(false);
+            }
+        });
+    }    
+    
+    @FXML
+    public void openWindowBack(ActionEvent e) {
+        openWindowWithOption("Principal.fxml");
+    }
+    
+    private void openWindowWithOption(String file) {
+         
+        Stage stage2 = (Stage) x0Entry.getScene().getWindow();
+        stage2.close();
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource(file));
+        } catch (IOException ex) {
+            Logger.getLogger(BiseccionGUIController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
+    }
+}
